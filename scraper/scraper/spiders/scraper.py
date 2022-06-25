f90772edd1137429b0d84cb6a64b210643889971
rajshree vats <2006508@kiit.ac.in>
2022-06-25 13:06:56 +0530

    word2vec model

--- scraper/scraper/spiders/scraper.py
+++ scraper/scraper/spiders/scraper.py
@@ -1,6 +1,6 @@
 import json
 import re
-
+import pandas as pd
 import scrapy
 from scrapy.http import HtmlResponse
 from bs4 import BeautifulSoup as bs
@@ -15,10 +15,51 @@ class ScrapeBajajFinserv(scrapy.Spider):
     custom_settings = {
         "USER_AGENT": "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36"
     }
-    links = [
+    
+    scraped = {}
+
+    def __del__(self):
+        with open(r"C:\Users\KIIT\OneDrive\Documents\scraper\scraper\scraped-data.json", "w") as f:
+            json.dump(self.scraped, f)
+
+    def start_requests(self):
+        links = {
         "https://www.bajajfinservmarkets.in/insurance/travel-insurance.html",
+        "https://www.bajajallianz.com/motor-insurance/car-insurance-online.html",
+        "https://www.bajajallianz.com/motor-insurance/two-wheeler-insurance-online.html",
+        "https://www.bajajallianz.com/motor-insurance/long-term-two-wheeler-insurance.html",
+        "https://www.bajajallianz.com/motor-insurance/third-party-car-insurance-online.html",
+        "https://www.bajajallianz.com/motor-insurance/two-wheeler-insurance-third-party.html",
+        "https://www.bajajallianz.com/motor-insurance/commercial-vehicle-insurance.html",
         "https://www.bajajfinservmarkets.in/insurance/cyber-security.html",
         "https://www.bajajfinservmarkets.in/insurance/health-insurance.html",
+        "https://www.bajajallianz.com/health-insurance-plans/family-health-insurance-india.html",
+        "https://www.bajajallianz.com/health-insurance-plans/health-insurance-infinity-plan.html",
+        "https://www.bajajallianz.com/health-insurance-plans/arogya-sanjeevani-standard-health-insurance-policy.html",
+        "https://www.bajajallianz.com/health-insurance-plans/corona-kavach-policy.html",
+        "https://www.bajajallianz.com/health-insurance-plans/top-up-health-insurance.html",
+        "https://www.bajajallianz.com/health-insurance-plans/criti-care.html",
+        "https://www.bajajallianz.com/health-insurance-plans/critical-Illness-insurance.html",
+        "https://www.bajajallianz.com/health-insurance-plans/individual-international-accident-insurance.html",
+        "https://www.bajajallianz.com/health-insurance-plans/health-insurance-for-senior-citizens.html",
+        "https://www.bajajallianz.com/health-insurance-plans/m-care-health-insurance-policy.html",
+        "https://www.bajajallianz.com/health-insurance-plans/saral-suraksha-bima-policy.html",
+        "https://www.bajajallianz.com/travel-insurance-online/individual-travel-insurance.html",
+        "https://www.bajajallianz.com/travel-insurance-online/family-travel-insurance.html",
+        "https://www.bajajallianz.com/travel-insurance-online/student-travel-insurance.html",
+        "https://www.bajajallianz.com/travel-insurance-online/travel-ace-plan.html",
+        "https://www.bajajallianz.com/travel-insurance-online/bharat-bhraman-domestic-travel-insurance.html",
+        "https://www.bajajallianz.com/travel-insurance-online/travel-asia-insurance.html",
+        "https://www.bajajallianz.com/travel-insurance-online/senior-citizen-travel-insurance.html",
+        "https://www.bajajallianz.com/travel-insurance-online/corporate-travel-insurance.html",
+        "https://www.bajajallianz.com/home-insurance/my-home-insurance.html",
+        "https://www.bajajallianz.com/home-insurance/householder-policy.html",
+        "https://www.bajajallianz.com/commercial-insurance/property-insurance.html",
+        "https://www.bajajallianz.com/commercial-insurance/marine-insurance.html",
+        "https://www.bajajallianz.com/commercial-insurance/liability-insurance.html",
+        "https://www.bajajallianz.com/commercial-insurance/financial-lines-insurance.html",
+        "https://www.bajajallianz.com/commercial-insurance/engineering-insurance.html",
+        "https://www.bajajallianz.com/commercial-insurance/employee-benefits-insurance.html",
         "https://www.bajajfinservmarkets.in/insurance/motor-insurance.html",
         "https://www.bajajfinservmarkets.in/insurance/ulip.html",
         "https://www.bajajfinservmarkets.in/insurance/term-life.html",
@@ -31,21 +72,14 @@ class ScrapeBajajFinserv(scrapy.Spider):
         "https://www.bajajfinservmarkets.in/insurance/car-insurance.html",
         "https://www.bajajfinservmarkets.in/insurance/endowment-policy.html",
         "https://www.bajajfinservmarkets.in/insurance/types-of-insurance.html",
-        "https://www.bajajfinservmarkets.in/insurance/sachet.html",
-    ]
-    scraped = {}
-
-    def __del__(self):
-        with open("C:\Users\KIIT\Desktop\Hackrx\crawler\scraper\scraper\scraped-data.json", "w") as f:
-            json.dump(self.scraped, f)
-
-    def start_requests(self):
-        for link in self.links:
+        "https://www.bajajfinservmarkets.in/insurance/sachet.html"
+        }
+        for link in links:
             sitemapLink = link.split(".html")[0] + "/sitemap.xml"
             yield scrapy.Request(url=sitemapLink, callback=self.parseSitemap)
-        for link in self.links:
+        for link in links:
             yield scrapy.Request(url=link, callback=self.parse)
-        for link in self.links:
+        for link in links:
             yield scrapy.Request(url=link, callback=self.parse_priority)
 
     def parseSitemap(self, response):
@@ -159,3 +193,7 @@ class ScrapeBajajFinserv(scrapy.Spider):
                 self.scraped[len(self.scraped)] = payload
             #with open("scraped-data.json", "w") as f:
                   #json.dump(content, f)
+
+pObj=pd.read_json(r"C:\Users\KIIT\OneDrive\Documents\scraper\scraper\scraped-data.json",orient='index')
+csvDataset=pObj.to_csv('dataset,csv',index=False)
+# print(csvDataset)
